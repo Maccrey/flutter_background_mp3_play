65609adacdccab505e9cf017b15ffa210174a3d3
Maccrey <m01071630214@gmail.com>
2024-12-30 13:03:19 +0900

    Enhance HomeScreen and BackgroundPlayController with additional audio control features and UI updates

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:white_noise_backgraund_play_module/viewmodel/background_play_controller.dart';
 
+// 백그라운드 화이트 노이즈 재생을 위한 홈 화면 위젯
 class HomeScreen extends StatefulWidget {
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+// 홈 화면의 상태를 관리하는 State 클래스
 class _HomeScreenState extends State<HomeScreen> {
+  // 백그라운드 오디오 재생을 제어하는 컨트롤러
   final BackgroundPlayController controller = BackgroundPlayController();
 
   @override
@@ -18,21 +21,87 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('White Noise'),
       ),
       body: Center(
+        // ValueListenableBuilder를 사용하여 재생 상태 변화 감지
         child: ValueListenableBuilder<bool>(
           valueListenable: controller.isPlaying,
           builder: (context, isPlaying, child) {
-            return ElevatedButton(
-              onPressed: () {
-                if (isPlaying) {
-                  controller.pauseBackgroundAudio();
-                } else {
-                  controller.playBackgroundAudio('assets/cafe-noise-32940.mp3');
-                }
-                setState(() {
-                  controller.isPlaying.value = !isPlaying;
-                });
-              },
-              child: Text(isPlaying ? 'Stop White Noise' : 'Play White Noise'),
+            return Column(
+              children: [
+                // 재생/일시정지 버튼
+                ElevatedButton(
+                  onPressed: () async {
+                    if (isPlaying) {
+                      // 현재 재생 중이면 일시정지
+                      await controller.pauseBackgroundAudio();
+                      controller.isPlaying.value = false;
+                    } else {
+                      await controller
+                          .playBackgroundAudio('assets/cafe-noise-32940.mp3');
+                      controller.isPlaying.value = true;
+                    }
+                  },
+                  child: Text(isPlaying ? '일시정지' : '재생'),
+                ),
+                // 반복 재생 버튼
+                ElevatedButton(
+                  onPressed: () {
+                    if (isPlaying) {
+                      controller.pauseBackgroundAudio();
+                      controller.isPlaying.value = false;
+                    } else {
+                      if (controller.currentAssetNotifier.value.isNotEmpty) {
+                        controller.resumeBackgroundAudio();
+                      } else {
+                        controller.playBackgroundLoopAudio(
+                            'assets/cafe-noise-32940.mp3');
+                      }
+                      controller.isPlaying.value = true;
+                    }
+                  },
+                  child: Text(isPlaying ? '일시정지' : '반복재생'),
+                ),
+                // 정지 버튼
+                ElevatedButton(
+                  onPressed: () async {
+                    await controller.stopBackgroundAudio();
+                    await controller.seekTo(Duration.zero);
+                    setState(() {
+                      controller.isPlaying.value = false;
+                      controller.currentAssetNotifier.value = '';
+                    });
+                  },
+                  child: const Text('정지'),
+                ),
+                // 재개 버튼
+                ElevatedButton(
+                    onPressed: () {
+                      controller.resumeBackgroundAudio();
+                    },
+                    child: Text('재개')),
+
+                // 재생 중인 오디오 파일 이름 표시
+                ValueListenableBuilder<String>(
+                  valueListenable: controller.currentAssetNotifier,
+                  builder: (context, currentAsset, child) {
+                    return Text(currentAsset);
+                  },
+                ),
+
+                // 오디오 파일 재생 시간을 실시간으로 표시
+                StreamBuilder<Duration>(
+                  stream: controller.positionStream(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final duration = snapshot.data!;
+                      return Text(
+                        '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      );
+                    }
+                    return const Text('00:00');
+                  },
+                ),
+              ],
             );
           },
         ),
--- lib/viewmodel/background_play_controller.dart
+++ lib/viewmodel/background_play_controller.dart
@@ -2,12 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
 class BackgroundPlayController {
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  final ValueNotifier<bool> isPlaying = ValueNotifier<bool>(false);
+  final AudioPlayer _audioPlayer = AudioPlayer(); // 오디오 플레이어 인스턴스
+  final ValueNotifier<bool> isPlaying =
+      ValueNotifier<bool>(false); // 재생 상태를 감지하는 ValueNotifier
+
+  // 현재 재생 중인 오디오 파일의 자산 경로를 감시하는 ValueNotifier
+  final ValueNotifier<String> currentAssetNotifier =
+      ValueNotifier<String>('재생 중인 오디오 없음');
   String? _currentAsset;
 
+  /// 지정된 에셋 경로의 오디오를 백그라운드에서 재생합니다.
+  ///
+  /// [assetPath]는 재생할 오디오 파일의 에셋 경로입니다.
+  ///
+  /// 현재 재생 중인 에셋과 다른 경우, 기존 재생을 중지하고 새로운 에셋을 로드합니다.
+  /// 루프 모드를 해제하고 오디오를 재생하며, 재생 상태를 true로 설정합니다.
+  ///
+  /// 에러가 발생하면 콘솔에 에러를 출력하고 예외를 다시 던집니다.
+  ///
+
+  /// 재생
   Future<void> playBackgroundAudio(String assetPath) async {
     try {
+      if (_currentAsset != assetPath) {
+        await _audioPlayer.stop();
+        await _audioPlayer.setAsset(assetPath);
+        _currentAsset = assetPath;
+        currentAssetNotifier.value = assetPath; // 현재 재생 중인 에셋 이름 갱신
+      }
+
+      await _audioPlayer.setLoopMode(LoopMode.off);
+      await _audioPlayer.play();
+      isPlaying.value = true;
+    } catch (e) {
+      debugPrint('Error playing audio: $e');
+      rethrow;
+    }
+  }
+
+  /// 반복재생
+  Future<void> playBackgroundLoopAudio(String assetPath) async {
+    try {
+      currentAssetNotifier.value = assetPath; // 현재 재생 중인 에셋 이름 갱신
       if (_currentAsset != assetPath) {
         await _audioPlayer.stop();
         await _audioPlayer.setAsset(assetPath);
@@ -24,6 +60,7 @@ class BackgroundPlayController {
     }
   }
 
+  /// 정지
   Future<void> stopBackgroundAudio() async {
     try {
       await _audioPlayer.stop();
@@ -32,8 +69,11 @@ class BackgroundPlayController {
     }
   }
 
-  void resumeBackgroundAudio() {
+  /// 재개
+  resumeBackgroundAudio() {
     try {
+      // currentAssetNotifier.value = _currentAsset ??
+      //     'resumeBackgroundAudio No audio playing'; // 현재 재생 중인 에셋 이름 갱신
       _audioPlayer.play();
       isPlaying.value = true;
     } catch (e) {
@@ -41,13 +81,29 @@ class BackgroundPlayController {
     }
   }
 
-  void pauseBackgroundAudio() {
+  /// 일시정지
+  pauseBackgroundAudio() {
     try {
       _audioPlayer.pause();
       isPlaying.value = false;
     } catch (e) {
       debugPrint('Error pausing audio: $e');
     }
+  }
+
+// 현재 재생 중인 오디오 파일의 재생 시간을 반환합니다.
+  currentAsset() {
+    return _currentAsset;
+  }
+
+// 현재 재생 중인 오디오 파일의 재생 시간을 반환합니다.
+  Stream<Duration> positionStream() {
+    return _audioPlayer.positionStream;
+  }
+
+// 현재 재생 중인 오디오 파일의 재생 시간을 반환합니다.
+  seekTo(Duration position) {
+    _audioPlayer.seek(position);
   }
 
   void dispose() {
