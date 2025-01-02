010fa9e872dd3ed0e5428cc7e8a2675f97cfd444
Maccrey <m01071630214@gmail.com>
2025-01-02 17:16:52 +0900

    Add SecondScreen and enhance BackgroundPlayController with playback state management

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:white_noise_backgraund_play_module/view/second_screen.dart';
 import 'package:white_noise_backgraund_play_module/viewmodel/background_play_controller.dart';
 
-// 백그라운드 화이트 노이즈 재생을 위한 홈 화면 위젯
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,103 +9,293 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-// 홈 화면의 상태를 관리하는 State 클래스
 class _HomeScreenState extends State<HomeScreen> {
-  // 백그라운드 오디오 재생을 제어하는 컨트롤러
   final BackgroundPlayController controller = BackgroundPlayController();
+  final Map<String, BackgroundPlayController> controllers = {
+    'cafe': BackgroundPlayController(),
+    'rain': BackgroundPlayController(),
+    'wind': BackgroundPlayController(),
+    'fire': BackgroundPlayController(),
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: const Text('White Noise'),
-      ),
-      body: Center(
-        // ValueListenableBuilder를 사용하여 재생 상태 변화 감지
-        child: ValueListenableBuilder<bool>(
-          valueListenable: controller.isPlaying,
-          builder: (context, isPlaying, child) {
-            return Column(
-              children: [
-                // 재생/일시정지 버튼
-                ElevatedButton(
-                  onPressed: () async {
-                    if (isPlaying) {
-                      // 현재 재생 중이면 일시정지
-                      await controller.pauseBackgroundAudio();
-                      controller.isPlaying.value = false;
-                    } else {
-                      await controller
-                          .playBackgroundAudio('assets/cafe-noise-32940.mp3');
-                      controller.isPlaying.value = true;
-                    }
-                  },
-                  child: Text(isPlaying ? '일시정지' : '재생'),
-                ),
-                // 반복 재생 버튼
-                ElevatedButton(
-                  onPressed: () {
-                    if (isPlaying) {
-                      controller.pauseBackgroundAudio();
-                      controller.isPlaying.value = false;
-                    } else {
-                      if (controller.currentAssetNotifier.value.isNotEmpty) {
-                        controller.resumeBackgroundAudio();
-                      } else {
-                        controller.playBackgroundLoopAudio(
-                            'assets/cafe-noise-32940.mp3');
-                      }
-                      controller.isPlaying.value = true;
-                    }
-                  },
-                  child: Text(isPlaying ? '일시정지' : '반복재생'),
-                ),
-                // 정지 버튼
-                ElevatedButton(
-                  onPressed: () async {
-                    await controller.stopBackgroundAudio();
-                    await controller.seekTo(Duration.zero);
-                    setState(() {
-                      controller.isPlaying.value = false;
-                      controller.currentAssetNotifier.value = '';
-                    });
-                  },
-                  child: const Text('정지'),
-                ),
-                // 재개 버튼
-                ElevatedButton(
-                    onPressed: () {
-                      controller.resumeBackgroundAudio();
-                    },
-                    child: Text('재개')),
-
-                // 재생 중인 오디오 파일 이름 표시
-                ValueListenableBuilder<String>(
-                  valueListenable: controller.currentAssetNotifier,
-                  builder: (context, currentAsset, child) {
-                    return Text(currentAsset);
-                  },
-                ),
-
-                // 오디오 파일 재생 시간을 실시간으로 표시
-                StreamBuilder<Duration>(
-                  stream: controller.positionStream(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final duration = snapshot.data!;
-                      return Text(
-                        '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      );
-                    }
-                    return const Text('00:00');
-                  },
-                ),
-              ],
-            );
-          },
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text(
+          'White Noise',
+          style: TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                    left: 16, bottom: 8), // Reduced bottom padding
+                child: Text(
+                  'Choose Your Ambient Sound',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 2),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      soundControlButton(
+                          '카페소리', 'assets/cafe-noise-32940.mp3', 'cafe'),
+                      soundControlButton('비소리',
+                          'assets/sound-of-falling-rain-145473.mp3', 'rain'),
+                      soundControlButton(
+                          '바람소리', 'assets/wind__artic__cold-6195.mp3', 'wind'),
+                      soundControlButton('장작소리',
+                          'assets/fireplace-with-crackling-sounds.mp3', 'fire'),
+                    ],
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const SecondScreen();
+                    }));
+                  },
+                  child: Text('다음페이지로 이동'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Widget soundControlButton(
+      String soundName, String soundFilePath, String key) {
+    return SizedBox(
+      width: double.infinity,
+      child: ValueListenableBuilder<PlaybackState>(
+        valueListenable: controllers[key]!.playbackState,
+        builder: (context, state, _) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(12, 0, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  soundName,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    StreamBuilder<Duration>(
+                      stream: controllers[key]!.positionStream(),
+                      builder: (context, snapshot) {
+                        final duration = snapshot.data ?? Duration.zero;
+                        return Text(
+                          '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}',
+                          style: TextStyle(fontSize: 16),
+                        );
+                      },
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        switch (state) {
+                          case PlaybackState.stopped:
+                            controllers[key]!
+                                .playBackgroundLoopAudio(soundFilePath);
+                            controllers[key]!.playbackState.value =
+                                PlaybackState.playing;
+                            break;
+                          case PlaybackState.playing:
+                            controllers[key]!.pauseBackgroundAudio();
+                            controllers[key]!.playbackState.value =
+                                PlaybackState.paused;
+                            break;
+                          case PlaybackState.paused:
+                            controllers[key]!.resumeBackgroundAudio();
+                            controllers[key]!.playbackState.value =
+                                PlaybackState.playing;
+
+                            break;
+                        }
+                      },
+                      icon: state == PlaybackState.stopped
+                          ? Row(
+                              children: [
+                                const Icon(
+                                  Icons.play_arrow,
+                                  color: Colors.blue,
+                                ),
+                                setVolumeButton(key),
+                              ],
+                            )
+                          : state == PlaybackState.playing
+                              ? Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.pause,
+                                      color: Colors.red,
+                                    ),
+                                    setVolumeButton(key),
+                                  ],
+                                )
+                              : Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.play_arrow,
+                                      color: Colors.red,
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        controllers[key]!.stopBackgroundAudio();
+                                        setState(() {
+                                          controllers[key]!
+                                              .playbackState
+                                              .value = PlaybackState.stopped;
+                                        });
+                                      },
+                                      icon: state == PlaybackState.paused
+                                          ? const Icon(
+                                              Icons.stop,
+                                              color: Colors.black54,
+                                            )
+                                          : Container(),
+                                    ),
+                                  ],
+                                ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget playbackDurationText() {
+    return StreamBuilder<Duration>(
+      stream: controller.positionStream(),
+      builder: (context, snapshot) {
+        final duration = snapshot.data ?? Duration.zero;
+        return Text(
+          '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}',
+          style: TextStyle(fontSize: 14),
+        );
+      },
+    );
+  }
+
+  //음량 컨트롤러
+  Widget volumeController(String key) {
+    return ValueListenableBuilder<double>(
+      valueListenable: controllers[key]!.volumeNotifier,
+      builder: (context, volume, _) {
+        return Slider(
+          value: volume,
+          onChanged: (value) {
+            controllers[key]!.setVolume(value);
+          },
+          min: 0,
+          max: 1,
+          divisions: 10,
+          label: volume.toStringAsFixed(1),
+        );
+      },
+    );
+  }
+
+  setVolumeButton(String key) {
+    return IconButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                width: 280,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Volume Control',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    volumeController(key),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () => Navigator.pop(context),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 12,
+                        ),
+                      ),
+                      child: const Text(
+                        'Close',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      icon: const Icon(Icons.volume_up),
     );
   }
 }
--- lib/viewmodel/background_play_controller.dart
+++ lib/viewmodel/background_play_controller.dart
@@ -1,112 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
-class BackgroundPlayController {
-  final AudioPlayer _audioPlayer = AudioPlayer(); // 오디오 플레이어 인스턴스
-  final ValueNotifier<bool> isPlaying =
-      ValueNotifier<bool>(false); // 재생 상태를 감지하는 ValueNotifier
+enum PlaybackState {
+  playing,
+  stopped,
+  paused,
+}
 
-  // 현재 재생 중인 오디오 파일의 자산 경로를 감시하는 ValueNotifier
+class BackgroundPlayController {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  final ValueNotifier<Map<Key, PlaybackState>> playbackStateMap =
+      ValueNotifier({});
+
+  final ValueNotifier<double> volumeNotifier = ValueNotifier(1.0);
+
+  ValueNotifier<PlaybackState> playbackState =
+      ValueNotifier<PlaybackState>(PlaybackState.stopped);
+
   final ValueNotifier<String> currentAssetNotifier =
       ValueNotifier<String>('재생 중인 오디오 없음');
   String? _currentAsset;
+  bool isPlaying = false;
 
-  /// 지정된 에셋 경로의 오디오를 백그라운드에서 재생합니다.
-  ///
-  /// [assetPath]는 재생할 오디오 파일의 에셋 경로입니다.
-  ///
-  /// 현재 재생 중인 에셋과 다른 경우, 기존 재생을 중지하고 새로운 에셋을 로드합니다.
-  /// 루프 모드를 해제하고 오디오를 재생하며, 재생 상태를 true로 설정합니다.
-  ///
-  /// 에러가 발생하면 콘솔에 에러를 출력하고 예외를 다시 던집니다.
-  ///
+  Future<void> playPause(String asset) async {
+    if (_currentAsset != asset) {
+      await _audioPlayer.stop();
+      await _audioPlayer.setAsset(asset);
+      _currentAsset = asset;
+      isPlaying = false;
+    }
 
-  /// 재생
-  Future<void> playBackgroundAudio(String assetPath) async {
-    try {
-      if (_currentAsset != assetPath) {
-        await _audioPlayer.stop();
-        await _audioPlayer.setAsset(assetPath);
-        _currentAsset = assetPath;
-        currentAssetNotifier.value = assetPath; // 현재 재생 중인 에셋 이름 갱신
-      }
-
-      await _audioPlayer.setLoopMode(LoopMode.off);
+    if (isPlaying) {
+      await _audioPlayer.pause();
+      isPlaying = false;
+    } else {
       await _audioPlayer.play();
-      isPlaying.value = true;
-    } catch (e) {
-      debugPrint('Error playing audio: $e');
-      rethrow;
+      isPlaying = true;
     }
   }
 
-  /// 반복재생
   Future<void> playBackgroundLoopAudio(String assetPath) async {
     try {
-      currentAssetNotifier.value = assetPath; // 현재 재생 중인 에셋 이름 갱신
+      currentAssetNotifier.value = assetPath;
       if (_currentAsset != assetPath) {
         await _audioPlayer.stop();
         await _audioPlayer.setAsset(assetPath);
         await _audioPlayer.setLoopMode(LoopMode.all);
         _currentAsset = assetPath;
       }
-      if (_audioPlayer.loopMode != LoopMode.all) {
-        await _audioPlayer.setLoopMode(LoopMode.all);
-      }
       await _audioPlayer.play();
-      isPlaying.value = true;
+      playbackState.value = PlaybackState.playing;
     } catch (e) {
       debugPrint('Error playing audio: $e');
     }
   }
 
-  /// 정지
   Future<void> stopBackgroundAudio() async {
     try {
       await _audioPlayer.stop();
+      await _audioPlayer.seek(Duration.zero);
+      playbackState.value = PlaybackState.stopped;
     } catch (e) {
       debugPrint('Error stopping audio: $e');
     }
   }
 
-  /// 재개
-  resumeBackgroundAudio() {
+  Future<void> resumeBackgroundAudio() async {
     try {
-      // currentAssetNotifier.value = _currentAsset ??
-      //     'resumeBackgroundAudio No audio playing'; // 현재 재생 중인 에셋 이름 갱신
-      _audioPlayer.play();
-      isPlaying.value = true;
+      await _audioPlayer.play();
+      playbackState.value = PlaybackState.playing;
     } catch (e) {
       debugPrint('Error resuming audio: $e');
     }
   }
 
-  /// 일시정지
-  pauseBackgroundAudio() {
+  Future<void> pauseBackgroundAudio() async {
     try {
-      _audioPlayer.pause();
-      isPlaying.value = false;
+      await _audioPlayer.pause();
+      playbackState.value = PlaybackState.paused;
     } catch (e) {
       debugPrint('Error pausing audio: $e');
     }
   }
 
-// 현재 재생 중인 오디오 파일의 재생 시간을 반환합니다.
-  currentAsset() {
-    return _currentAsset;
+  String? getCurrentAsset() => _currentAsset;
+
+  Stream<Duration> getPositionStream() => _audioPlayer.positionStream;
+
+  Future<void> seekTo(Duration position) async {
+    await _audioPlayer.seek(position);
   }
 
-// 현재 재생 중인 오디오 파일의 재생 시간을 반환합니다.
+//재생시간 표시
   Stream<Duration> positionStream() {
     return _audioPlayer.positionStream;
   }
 
-// 현재 재생 중인 오디오 파일의 재생 시간을 반환합니다.
-  seekTo(Duration position) {
-    _audioPlayer.seek(position);
+//setVolume
+  Future<void> setVolume(double volume) async {
+    await _audioPlayer.setVolume(volume);
+    volumeNotifier.value = volume;
   }
 
   void dispose() {
+    playbackState.dispose();
+    currentAssetNotifier.dispose();
     _audioPlayer.dispose();
   }
 }
