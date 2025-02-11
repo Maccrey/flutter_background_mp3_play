003a75bb05a226fbe6497fc9777aa7b15de82566
Maccrey <m01071630214@gmail.com>
2025-02-11 14:08:43 +0900

    리팩토링 작업 완료

--- lib/view/first_screen.dart
+++ lib/view/first_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:white_noise_backgraund_play_module/view/whitenoise_player_screen.dart';
-import '../model/audio_model.dart';
-import '../services/audio_service.dart';
 import 'drawer_white_noise_controller.dart';
 
 class FirstScreen extends ConsumerWidget {
@@ -10,100 +8,10 @@ class FirstScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final audioList =
-        ref.watch(multiAudioViewModelProvider); // Get current playback states
-
-    // Filter the list to get all playing audios
-    final playingAudios = audioList
-        .where((audio) => audio.playbackState == PlaybackState.playing)
-        .toList(); // Get all playing audios
-
     return Scaffold(
       appBar: AppBar(
           title: const Text('FirstScreen'), automaticallyImplyLeading: false),
       drawer: DrewerWhiteNoiseController(),
-      // drawer: Container(
-      //   height: MediaQuery.of(context).size.height * 0.85,
-      //   child: Drawer(
-      //     backgroundColor: Colors.white,
-      //     width: MediaQuery.of(context).size.width * 0.20,
-      //     child: Column(
-      //       children: [
-      //         Expanded(
-      //             child: playingAudios.isNotEmpty
-      //                 ? ListView.builder(
-      //                     itemCount: playingAudios.length,
-      //                     itemBuilder: (context, index) {
-      //                       final playingAudio = playingAudios[index];
-
-      //                       return Padding(
-      //                         padding:
-      //                             const EdgeInsets.symmetric(vertical: 8.0),
-      //                         child: Column(
-      //                           children: [
-      //                             GestureDetector(
-      //                               onTap: () {
-      //                                 ref
-      //                                     .read(multiAudioViewModelProvider
-      //                                         .notifier)
-      //                                     .togglePlayback(
-      //                                         playingAudio.assetPath);
-      //                               },
-      //                               child: ClipOval(
-      //                                 child: Image.asset(
-      //                                   playingAudio.imagePath,
-      //                                   width:
-      //                                       MediaQuery.of(context).size.width *
-      //                                           0.15,
-      //                                   height:
-      //                                       MediaQuery.of(context).size.width *
-      //                                           0.15,
-      //                                   fit: BoxFit.cover,
-      //                                 ),
-      //                               ),
-      //                             ),
-      //                             const SizedBox(height: 8),
-      //                             Text(
-      //                               playingAudio.name,
-      //                               style: const TextStyle(fontSize: 16),
-      //                             ),
-      //                           ],
-      //                         ),
-      //                       );
-      //                     },
-      //                   )
-      //                 : Center(
-      //                     child: InkWell(
-      //                       onTap: () => Navigator.push(
-      //                           context,
-      //                           MaterialPageRoute(
-      //                               builder: (context) => WhiteNoiseScreen())),
-      //                       child: Column(
-      //                         mainAxisAlignment: MainAxisAlignment.center,
-      //                         children: "화이트 노이즈가 재생되고 있지 않습니다"
-      //                             // "White noise is not playing"
-      //                             .split('')
-      //                             .map((char) {
-      //                           return Text(
-      //                             char,
-      //                             style: TextStyle(fontSize: 14),
-      //                           );
-      //                         }).toList(),
-      //                       ),
-      //                     ),
-      //                   )),
-      //         Text(
-      //           'Stop',
-      //           style: TextStyle(
-      //               fontSize: 16,
-      //               fontWeight: FontWeight.bold,
-      //               color: Colors.black54),
-      //         ),
-      //         SizedBox(height: 20),
-      //       ],
-      //     ),
-      //   ),
-      // ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
