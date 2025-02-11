93f22f99a907fb5a1833737cb507c6a0b98ecc04
Maccrey <m01071630214@gmail.com>
2025-02-11 13:53:01 +0900

    build.gradle minSdk = 24로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:white_noise_backgraund_play_module/view/first_screen.dart';
 
-import 'view/whitenoise_player_screen.dart';
-
 void main() {
   runApp(
     ProviderScope(
